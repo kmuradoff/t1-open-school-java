72153264213f54ab2c9e8077cf9e495b46fc110e
Kamal Muradov <muradoffk@gmail.com>
2025-02-19 11:17:22 +0300

    Init project

---
+++ src/main/java/org/kmuradoff/openschooljava/OpenSchoolJavaTask1Application.java
@@ -0,0 +1,13 @@
+package org.kmuradoff.openschooljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolJavaTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolJavaTask1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/kmuradoff/openschooljava/OpenSchoolJavaTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kmuradoff.openschooljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolJavaTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
