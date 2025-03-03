116c435e2dbc9c62c2fa1262fa5c3d31190f4a52
Kamal Muradov <muradoffk@gmail.com>
2025-03-03 11:25:05 +0300

    Refactored kafka listener type from single to batch

--- src/main/java/org/kmuradoff/openschooljava/adapter/in/kafka/KafkaConsumerService.java
+++ src/main/java/org/kmuradoff/openschooljava/adapter/in/kafka/KafkaConsumerService.java
@@ -7,40 +7,34 @@ import org.kmuradoff.openschooljava.adapter.out.postgres.dto.TaskStatus;
 import org.kmuradoff.openschooljava.application.domain.dto.EmailNotificationDto;
 import org.kmuradoff.openschooljava.application.port.in.NotificationService;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumerService {
+
     private final NotificationService notificationService;
 
-    @RetryableTopic(
-            attempts = "5",
-            backoff = @Backoff(delay = 5000, multiplier = 2),
-            kafkaTemplate = "kafkaTemplate"
-    )
     @KafkaListener(topics = "task-status", groupId = "task-id")
-    public void handleStatusUpdate(ConsumerRecord<String, TaskStatus> record, Acknowledgment ack) {
-        var id = record.key();
-        var status = record.value();
-
+    public void handleStatusUpdate(List<ConsumerRecord<String, TaskStatus>> records, Acknowledgment ack) {
+        log.info("Received {} messages from Kafka", records.size());
         try {
-            log.info("Received message from Kafka: {}", record);
-
-            var notification = EmailNotificationDto.builder()
-                    .taskId(id)
-                    .taskStatus(status.toString())
-                    .build();
-
-            notificationService.sendEmailNotification(notification);
-
+            for (ConsumerRecord<String, TaskStatus> record : records) {
+                String id = record.key();
+                TaskStatus status = record.value();
+                EmailNotificationDto notification = EmailNotificationDto.builder()
+                        .taskId(id)
+                        .taskStatus(status.toString())
+                        .build();
+                notificationService.sendEmailNotification(notification);
+            }
             ack.acknowledge();
         } catch (Exception e) {
-            log.error("Error while sending email. Will retry.", e);
+            log.error("Error while sending email notifications. Will retry entire batch.", e);
             throw e;
         }
     }
